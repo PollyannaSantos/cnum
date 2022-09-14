932f42cf9252c2cab2049a37adcb5766de568e86
Pollyanna dos Santos <111003060+PollyannaSantos@users.noreply.github.com>
2022-09-14 16:23:59 +0000

    Aula 6 (Concluída)

--- 6/main.jl
+++ 6/main.jl
@@ -4,7 +4,7 @@ error = 10^-3
 
 # Método de Jacobi
 
-function jacobi(A, B, k)
+function jacobi(A, B, k,error)
   n = size(B,1)
   X = zeros(n)
   K = zeros(n)
@@ -29,7 +29,7 @@ function jacobi(A, B, k)
 
 # Método de Gauss-Seidel
 
-function seidel(A, B, k)
+function seidel(A, B, k,error)
   n = size(B,1)
   X = zeros(n)
   K = zeros(n)
@@ -60,38 +60,108 @@ A = [5 1 1 ;
 
 B = [50 ; 10 ; -30]
 
-# Jacobi
 
-X = jacobi(A, B, 10)
+
+X = A \ B
 println(X)
 println( )
+
+println("Exercício 4.7.3:")
+
+# Jacobi
+
+X = jacobi(A, B, 100,error)
+println(X)
 
 # Seidel
 
-X = seidel(A, B, 100)
+X = seidel(A, B, 100,error)
 println(X)
 println( )
+
 
 println("Exercício 4.7.5")
 
-A = [1 10 3 ;
-     2 1 4 ;
-     4 0 1]
+A = [4 0 1 ;
+     1 10 3 ;
+     2 1 4]
 
-B = [27 ; 12 ; 6]
+B = [6 ; 27 ; 12]
 
 # Jacobi
-X = jacobi(A, B, 10)
+X = jacobi(A, B, 100,error)
 println(X)
-println( )
 
 # Seidel
 
-X = seidel(A, B, 100)
+X = seidel(A, B, 100,error)
 println(X)
 println( )
 
-println("Exercício 4.9.1")
-# Caso a
-# Caso b
-println("Exercício 4.9.2")
+println("Exercício 4.9.1 e 4.9.2")
+
+A = [1 0 0 0 0;
+     (1/2) ((-1/2)+(-1/2)+(-1/100)) (1/2) 0 0;
+     0 (1/2) ((-1/2)+(-1/2)+(-1/100)) (1/2) 0;
+     0 0 (1/2) ((-1/2)+(-1/2)+(-1/100)) (1/2);
+     0 0 0 (1/2) ((-1/2)+(-1/50))]
+
+B = [127 ; 0 ; 0 ; 0 ; 0]
+
+X = A \ B
+println(X)
+
+X = jacobi(A,B,100,error)
+println(X)
+
+X = seidel(A,B,100,error)
+println(X)
+
+A = [1 0 0 0 0;
+     (1/2) ((-1/2)+(-1/2)+(-1/50)) (1/2) 0 0;
+     0 (1/2) ((-1/2)+(-1/2)+(-1/100)) (1/2) 0;
+     0 0 (1/2) ((-1/2)+(-1/2)+(-1/100)) (1/2);
+     0 0 0 (1/2) ((-1/2)+(-1/100))]
+
+B = [127 ; 0 ; 0 ; 0 ; 0]
+
+X = A \ B
+println(X)
+
+X = jacobi(A,B,100,error)
+println(X)
+
+X = seidel(A,B,100,error)
+println(X)
+
+A = [((-1/2)+(-1/2)+(-1/100)) (1/2) 0 0;
+     (1/2) ((-1/2)+(-1/2)+(-1/100)) (1/2) 0;
+     0 (1/2) ((-1/2)+(-1/2)+(-1/100)) (1/2);
+     0 0 (1/2) ((-1/2)+(-1/50))]
+
+B = [(-127/2) ; 0 ; 0 ; 0]
+
+X = A \ B
+println(X)
+
+X = jacobi(A,B,100,error)
+println(X)
+
+X = seidel(A,B,100,error)
+println(X)
+
+A = [((-1/2)+(-1/2)+(-1/50)) (1/2) 0 0;
+     (1/2) ((-1/2)+(-1/2)+(-1/100)) (1/2) 0;
+     0 (1/2) ((-1/2)+(-1/2)+(-1/100)) (1/2);
+     0 0 (1/2) ((-1/2)+(-1/100))]
+
+B = [(-127/2) ; 0 ; 0 ; 0]
+
+X = A \ B
+println(X)
+
+X = jacobi(A,B,100,error)
+println(X)
+
+X = seidel(A,B,100,error)
+println(X)
